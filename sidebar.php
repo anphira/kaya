f60b385d1a805e5e4ae2c016f2934347a99449c7
anphira <info@anphira.com>
2017-08-24 11:47:04 -0400

    version 0.5
    = 0.5 - Aug 24 2017 =
    * modify social to open in new window
    * add hero banner for inside pages
    * add everywhere top and bottom sidebars to allow content to be added
    to every sidebar above and below the template specific sidebars
    * fix: single sidebar displays correctly on single posts

--- sidebar.php
+++ sidebar.php
@@ -4,16 +4,29 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
+ * @author  Anphira
+ * @since   0.1
  * @package Kaya
+ * @version 0.5
  */
 
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary">
-	<?php if ( (get_post_type() == 'post' || is_home()) && is_active_sidebar( 'posts-sidebar' ) ) {
+	<?php 
+	if(is_active_sidebar('everywhere-top-sidebar')) {
+		dynamic_sidebar('everywhere-top-sidebar');
+	}
+	if ( is_single() && is_active_sidebar( 'single-post-sidebar' ) ) {
+		dynamic_sidebar( 'single-post-sidebar' );
+	} 
+	elseif ( (get_post_type() == 'post' || is_home()) && is_active_sidebar( 'posts-sidebar' ) ) {
 		dynamic_sidebar( 'posts-sidebar' );
-	} ?>
-	<?php if ( get_post_type() == 'page' && is_active_sidebar( 'pages-sidebar' ) ) {
+	} 
+	elseif ( get_post_type() == 'page' && is_active_sidebar( 'pages-sidebar' ) ) {
 		dynamic_sidebar( 'pages-sidebar' );
+	}
+	if(is_active_sidebar('everywhere-bottom-sidebar')) {
+		dynamic_sidebar('everywhere-bottom-sidebar');
 	} ?>
 </aside><!-- #secondary -->
